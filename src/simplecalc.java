dbab92ce8e42cd5d7155b1461e373feb3629f9f4
Laplace0021 <gol.d.jibriel@gmail.com>
2024-10-02 18:22:31 +0700

    Change to cumulative calculator and add (=) operation

--- src/simplecalc.java
+++ src/simplecalc.java
@@ -1,67 +1,91 @@
 import java.util.Scanner ;
-import java.util.InputMismatchException;
+
 
 public class simplecalc {
     public static void main(String[] args) throws Exception {
         Scanner number = new Scanner(System.in);
         String b;
         double a, c, d;
-        System.out.println("Program ini hanya dapat menjalankan operasi simple saja dan berlaku untuk 2 buah variabel");
-        System.out.print("masukan angka pertama (gunakan . sebagai pengganti , jika angka yang dimasukan adalah desimal) : ");
+        System.out.println("Kalkulator kumulatif");
+        System.out.println("" );
+        System.out.print("masukan angka : ");
         a = number.nextDouble();
-        System.out.print("masukan operasi yang anda ingin jalankan (+,-,*,/,%) : " );
+        c = 1;
+        do {  System.out.println("" );
+        System.out.println("Silakan pilih : " );
+        System.out.println("(+)" );
+        System.out.println("(-)" );
+        System.out.println("(*)" );
+        System.out.println("(/)" );
+        System.out.println("(%)" );
+        if (c!=0){
+            System.out.println("(=)");
+        }
         b = number.next();
-        System.out.print("Masukan angka kedua (gunakan . sebagai pengganti , jika angka yang dimasukan adalah desimal) : ");
-        c = number.nextDouble();
+        
         
 
  // Check if inputs are NaN (Not a Number)
- if (Double.isNaN(a) || Double.isNaN(c)) {
+ if (c==0) {
     System.out.println("Masukan angka saja");
 } else {
     // Check if the operation is valid
-    if (b.equals("+") || b.equals("-") || b.equals("*") || b.equals("/") || b.equals("%")) {
+    if (b.equals("+") || b.equals("=") || b.equals("-") || b.equals("*") || b.equals("/") || b.equals("%")) {
 
         switch (b) {
             case "+":
-                d = a + c;
-                System.out.println("Hasil: " + d);
+                System.out.print("Masukan angka : ");
+                c = number.nextDouble();
+                a = a + c;
+                System.out.println("Hasil: " + a);
                 break;
 
             case "-":
-                d = a - c;
-                System.out.println("Hasil: " + d);
+                System.out.print("Masukan angka : ");
+                c = number.nextDouble();
+                a = a - c;
+                System.out.println("Hasil: " + a);
                 break;
 
             case "*":
-                d = a * c;
-                System.out.println("Hasil: " + d);
+                System.out.print("Masukan angka : ");
+                 c = number.nextDouble();
+                a = a * c;
+                System.out.println("Hasil: " + a);
                 break;
 
             case "/":
+                System.out.print("Masukan angka : ");
+                c = number.nextDouble();
                 if (c != 0) {
-                    d = a / c;
-                    System.out.println("Hasil: " + d);
+                    a = a / c;
+                    System.out.println("Hasil: " + a);
                 } else {
                     System.out.println("Error: Tidak bisa membagi dengan 0");
                 }
                 break;
 
             case "%":
+                System.out.print("Masukan angka : ");
+                c = number.nextDouble();
                 if (c != 0) {
-                    d = a % c;
-                    System.out.println("Hasil: " + d);
+                    a = a % c;
+                    System.out.println("Hasil: " + a);
                 } else {
                     System.out.println("Error: Tidak bisa modulus dengan 0");
                 }
+                break;
+            case "=":
+                a = a + c;
+                System.out.println("Hasil: " +a);
                 break;
         }
     } else {
         System.out.println("Silakan masukan operasi yang tersedia");
     }
 }
-
-
-number.close();
+            
+        } while (true);
+       
 }
 }
