2e1b252ae7bd6e3dbfd4782c526e0d1456e28f13
Laplace0021 <gol.d.jibriel@gmail.com>
2024-10-14 11:35:35 +0700

    Pertemuan ke 4

---
+++ src/Pertemuan4.java
@@ -0,0 +1,15 @@
+public class Pertemuan4 {
+    // 0 1 1 2 3 5 8 13 21 34 55 89
+    //
+    // c=a+b
+    //
+    public static void main(String[] args) {
+        for (int a = 1; a <= 3; a++) {
+            for (int b = 1; b <= 5; b++) {
+                System.out.print(a + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
---
+++ src/pengulanganwhile.java
@@ -0,0 +1,11 @@
+public class pengulanganwhile {
+    public static void main(String[] args) {
+    int i=10;
+    int c=0;
+    do {
+        System.out.println(c);
+        c++;
+    }while(i!=c);
+    
+    }
+}
