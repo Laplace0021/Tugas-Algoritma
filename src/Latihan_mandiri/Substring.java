bd550813a063b81232637fb3caab2ed016560261
Laplace0021 <gol.d.jibriel@gmail.com>
2024-11-03 19:37:57 +0700

    uncomplete

---
+++ src/Latihan_mandiri/Substring.java
@@ -0,0 +1,13 @@
+package Latihan_mandiri;
+import java.util.Scanner;
+
+public class Substring {
+    
+
+    public int lengthOfLongestSubstring(String s) {
+        Scanner input = new Scanner(System.in);
+        String kata = input.Next();
+        String huruf = kata.Substring;
+        System.out.println(huruf);
+    }
+}
