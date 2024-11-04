27ab00af7c3d14594b92c649e1f23af7dc011c3e
Laplace0021 <gol.d.jibriel@gmail.com>
2024-11-04 11:24:04 +0700

    Program menghitung total gaji

---
+++ src/Tugas_sesi_7/GajidanLembur.java
@@ -0,0 +1,23 @@
+package Tugas_sesi_7;
+import java.util.Scanner;
+
+public class GajidanLembur {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Program penghitung total gaji");
+        System.out.print("Masukan gaji anda: ");
+        int gaji = sc.nextInt();
+        System.err.print("Masukan total lembur(jam): ");
+        int jamLembur = sc.nextInt();
+        int lemburLebih  = 0;
+        if (jamLembur>4){
+            lemburLebih = jamLembur - 4;
+            jamLembur = 4;
+        }
+
+        int UpahLembur = (int) Math.ceil(gaji/173);
+        int totalGaji = gaji + (jamLembur*UpahLembur) + (lemburLebih*2*UpahLembur);
+        System.out.println("Total Gaji: "+totalGaji);
+        sc.close();
+    }    
+}
